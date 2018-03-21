13ecf2cd4f95886ac114d1cc7baec4797756da9d
James Taylor <35105264+jtatfranklin@users.noreply.github.com>
2018-03-21 08:13:53 -0500

    creation
    Initial upload to GitHub

---
+++ footer.php
@@ -0,0 +1,32 @@
+ <?php
+ echo "
+ <!-- Footer -->
+          <div class="row">
+		  
+            <div class="col-md-6">		  
+              <ul class="list-inline quicklinks">
+                <li class="list-inline-item"> <a class="text-white"  href="#">Legal</a>&nbsp;&nbsp; </li>
+                <li class="list-inline-item"> <a class="text-white"  href="#">Staff&nbsp;Login</a> </li>
+              </ul>		  
+			</div>
+			
+            <div class="col-md-6">
+                    <ul class="list-inline banner-social-buttons">
+					    <li class="list-inline-item">
+                            <a href="https://www.facebook.com/franklinassociates" target="_blank" class="btn btn-secondary btn-sm"><i class="fa fa-facebook fa-fw"></i> <span class="network-name">Facebook</span></a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="https://twitter.com/franklin_assoc" target="_blank" class="btn btn-secondary btn-sm"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="https://www.linkedin.com/company/franklin-associates" target="_blank" class="btn btn-secondary btn-sm"><i class="fa fa-linkedin fa-fw"></i> <span class="network-name">Linkedin</span></a>
+                        </li>
+                    </ul>
+            </div>
+			
+			</div>
+
+        <div class="row">
+            <p class="m-0 text-center text-white"></br>Copyright &copy; Franklin Associates, LLC 2017</p>
+        </div>
+	  ?>
